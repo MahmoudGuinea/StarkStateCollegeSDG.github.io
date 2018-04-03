91c7c09f8a8f18222f99a7f1e8345a797ee40be9
QualebTheTurtle <caleb.wagner365@gmail.com>
2018-04-02 23:50:04 -0400

    stuff

--- scripts/navigation.js
+++ scripts/navigation.js
@@ -2,22 +2,26 @@
 *NavData gets json object through jquery
 */
 function NavData(){
-	//jquery get json function
-	//innser function gets elements array of objects within the json
-	//then for each object it gets a name and link variable
-	//uses these variables in the print data function
+	/*
+	*jquery get json function
+	*
+	*refrence  https://www.youtube.com/watch?v=j-S5MBs4y0Q
+	*
+	*inner function gets elements array of objects within the json
+	*for each object it gets a name and link variable
+	*uses these variables in the print data function
+	*/
 	$.getJSON('json/navigation.json', function(data){
 		 $(data.elements).each(function(index, value){
-		 	console.log(value);
-
+		 	//console.log(value);
 		 	var name = value.name;
-		 	var id = value.name;
+		 	//regex to remove spaces
+		 	var id = name.replace(/\s/g, '');
 		 	var link = value.link;
 
 		 	printNav(name, id, link);
 		 });
 	});
-
 }
 
 /*
@@ -32,7 +36,7 @@ function printNav(name, id, link){
 
 	/*
 	*searches the HTML for the first tag with the id = "nav-links"
-	*then sets the last child element of the element to the table HTML object
+	*then sets the last child element of the element to the HTML a object
 	*/	
 	document.getElementById("nav-links").appendChild(links);
 }
